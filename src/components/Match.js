069126345488be44d16b80cfd570d4781135b25c
asitsi <41507591+asitsi@users.noreply.github.com>
2021-06-07 20:01:39 +0530

    Update Match.js

--- src/components/Match.js
+++ src/components/Match.js
@@ -8,7 +8,7 @@ const Match = (props) => {
   const [value, setvalue] = useState([]);
   const getData = async () => {
     const res = await fetch(
-      "https://51.143.173.5/api/developer/matchapi.php?Action=listMarketTypes&EventID=" +
+      "http://51.143.173.5/api/developer/matchapi.php?Action=listMarketTypes&EventID=" +
         Id +
         "&sportId=" +
         EventId
